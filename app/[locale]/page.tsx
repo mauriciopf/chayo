8259f26b8ded911ea256bb1f39ae2f52ba32dbb4
a6003927 <a6003927@bestbuy.com>
2025-07-17 00:18:24 -0400

    Remove OAuthRedirectHandler from landing page - no longer needed with OTP auth

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,15 +2,9 @@
 
 import { Suspense } from 'react'
 import ChayoAIHome from '@/components/ChayoAIHome'
-import OAuthRedirectHandler from '@/components/OAuthRedirectHandler'
 
 export default function HomePage() {
   return (
-    <>
-      <Suspense fallback={null}>
-        <OAuthRedirectHandler />
-      </Suspense>
-      <ChayoAIHome />
-    </>
+    <ChayoAIHome />
   )
 }
